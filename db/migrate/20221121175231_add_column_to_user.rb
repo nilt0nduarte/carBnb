ee3a8123c73f2567142831fe9df1b37a03ad6e49
Josenilton Duarte <n8zion@gmail.com>
2022-11-21 14:57:33 -0300

    devise changes

---
+++ db/migrate/20221121175231_add_column_to_user.rb
@@ -0,0 +1,7 @@
+class AddColumnToUser < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :phone_number, :string
+  end
+end
