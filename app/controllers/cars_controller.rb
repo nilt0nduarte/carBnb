054ad3ac24986ee97ab9ab68fe2e830f1de3c093
Josenilton Duarte <n8zion@gmail.com>
2022-11-21 18:11:51 -0300

    18 11

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -7,6 +7,17 @@ class CarsController < ApplicationController
   end
 
   def create
-    @car = Car.new
+    @car = Car.new(car_params)
+    if @car.save
+      redirect_to car_path(@car)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:model, :brand, :year, :price_per_day)
   end
 end
