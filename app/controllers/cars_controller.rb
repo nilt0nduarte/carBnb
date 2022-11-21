2fc3d11d462194e09b80757aa729c0d6c2f229ce
Felipe Dottling <felipedott@gmail.com>
2022-11-21 14:23:00 -0500

    user creating

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,4 +1,7 @@
 class CarsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+
   def index
   end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+  # def new
+  #   @user = User.new
+  # end
+
+  # def create
+  #   @user = User.new(user_params)
+  #   if @user.save
+  #     redirect_to @user, notice: "user was successfully created."
+  #   else
+  #     render :new, status: :unprocessable_entity
+  #   end
+  # end
+
+  # private
+
+  # def user_params
+  #   params.require(:fuser).permit(:first_name, :last_name, :phone_number, :email, :number_of_guests)
+  # end
+
 end
