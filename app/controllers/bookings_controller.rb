0df26f4bc5257dd5ec7e7ebde4f3801aa8db892a
Felipe Dottling <felipedott@gmail.com>
2022-11-22 13:33:33 -0500

    finalizar show page carousel

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,3 @@
 class BookingsController < ApplicationController
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: "cars#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :cars do
