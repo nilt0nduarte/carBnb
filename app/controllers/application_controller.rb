64782b5bf00a0d8ef4a1f29c5b0297a53634099e
tadeu tovar <tadeutovar@hotmail.com>
2022-11-22 14:44:31 -0300

    finish my cars, missing css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,8 +14,11 @@ class ApplicationController < ActionController::Base
   include Pundit::Authorization
 
   # Pundit: allow-list approach
-  after_action :verify_authorized, except: :index, unless: :skip_pundit?
-  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+  after_action :verify_authorized, except: [:index, :my_cars], unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: [:index, :my_cars], unless: :skip_pundit?
+
+
+
 
   # Uncomment when you *really understand* Pundit!
   # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
