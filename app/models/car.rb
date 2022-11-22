cc216a9bbb1a17c8e7b5da35b4c15516344e4a6a
Felipe Dottling <felipedott@gmail.com>
2022-11-21 21:18:20 -0500

    added hover and other small changes to cards and cars

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,12 @@
 class Car < ApplicationRecord
   belongs_to :user
+
+  has_many :reservations
+
   has_many_attached :photos
+
+  validates :model, presence: true
+  validates :brand, presence: true
+  validates :year, presence: true
+  validates :price_per_day, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, presence: true, length: { in: 2..10 }
+  validates :last_name, presence: true, length: { in: 2..20 }
+  validates :phone_number, presence: true, length: { in: 5..10 }
 end
