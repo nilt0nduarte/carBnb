81d7f485c5253f32147e6243d1e6d0b98cfa1106
tadeu tovar <tadeutovar@hotmail.com>
2022-11-23 16:09:41 -0300

    ...

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -9,9 +9,9 @@ class Booking < ApplicationRecord
                        comparison: { greater_than: :start_date, message: "can't book a car in the past." }
 
 
-  validate :other_booking_overlap
+  validate :other_booking_overlap_start_date, :other_booking_overlap_end_date
 
-  def other_booking_overlap
+  def other_booking_overlap_start_date
     # run this validation if the other ones did pass
     if errors.blank?
       other_bookings = car.bookings
@@ -21,6 +21,19 @@ class Booking < ApplicationRecord
       end
       is_overlapping = !overlapping_bookings.empty?
       errors.add(:start_date, "Picked date is not available") if is_overlapping
+    end
+  end
+
+  def other_booking_overlap_end_date
+    # run this validation if the other ones did pass
+    if errors.blank?
+      other_bookings = car.bookings
+      overlapping_bookings = other_bookings.select do |other_booking|
+        # period.overlaps?(other_booking.period)
+        (other_booking.start_date..other_booking.end_date).to_a.include? self.end_date
+      end
+      is_overlapping = !overlapping_bookings.empty?
+      errors.add(:end_date, "Picked date is not available") if is_overlapping
     end
   end
 
