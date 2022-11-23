67cde26bef2dfa77e2f365222b85224eee6a24b6
Josenilton Duarte <n8zion@gmail.com>
2022-11-23 14:18:00 -0300

    dependent destroy and little changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :bookings, through: :cars
+  has_many :cars, dependent: :destroy
 
   validates :first_name, presence: true, length: { in: 2..10 }
   validates :last_name, presence: true, length: { in: 2..20 }
