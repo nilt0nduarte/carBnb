58a26d35ac179ad49f1658f4b5dca949a998c07e
Felipe Dottling <felipedott@gmail.com>
2022-11-23 11:12:09 -0500

    avatar prototipo

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,10 @@ class User < ApplicationRecord
 
   has_many :bookings, through: :cars
 
+  has_one_attached :photo
+
   validates :first_name, presence: true, length: { in: 2..10 }
   validates :last_name, presence: true, length: { in: 2..20 }
   validates :phone_number, presence: true, length: { in: 5..10 }
+
 end
