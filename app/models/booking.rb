65cf541c2065136e2374314e445a3c904d721e02
tadeu tovar <tadeutovar@hotmail.com>
2022-11-23 15:24:06 -0300

    start moduel

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -7,4 +7,17 @@ class Booking < ApplicationRecord
 
   validates :end_date, presence: true,
                        comparison: { greater_than: :start_date, message: "can't book a car in the past." }
+
+
+  def other_booking_overlap
+  # run this validation if the other ones did pass
+  if errors.blank?
+    other_bookings = restaurant.bookings
+    overlapping_bookings = other_bookings.select do |other_booking|
+      period.overlaps?(other_booking.period)
+    end
+    is_overlapping = !overlapping_bookings.empty?
+    errors.add(:base, "Picked date is not available") if is_overlapping
+  end
+end
 end
